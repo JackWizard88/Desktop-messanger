b596d3b93275c82384c172a2eda57451d91208da
Andrei Krylov <krilovavit@mail.ru>
2020-04-06 13:29:11 +0300

    цензура + история сообщений + отображение времени сообщений

---
+++ Commands/src/ru/geekbrains/java2/commands/command/ChangeNicknameCommand.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.java2.commands.command;
+
+import java.io.Serializable;
+
+public class ChangeNicknameCommand implements Serializable {
+
+    private final String login;
+    private String username;
+
+    public ChangeNicknameCommand(String login, String username) {
+        this.login = login;
+        this.username = username;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
---
+++ Commands/src/ru/geekbrains/java2/commands/command/ErrorCommand.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.java2.commands.command;
+
+import java.io.Serializable;
+
+public class ErrorCommand implements Serializable {
+    private final String errorMessage;
+
+    public ErrorCommand(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}

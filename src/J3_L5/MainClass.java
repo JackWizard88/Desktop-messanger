8145eac300f78b79e7a3805a3fe1da1948b6443d
Andrei Krylov <krilovavit@mail.ru>
2020-04-10 15:16:19 +0300

    втф

--- src/J3_L5/MainClass.java
+++ src/J3_L5/MainClass.java
@@ -4,16 +4,16 @@ import java.util.concurrent.*;
 
 public class MainClass {
 
-    public static final int CARS_COUNT = 10;
+    public static final int CARS_COUNT = 4;
     public static final int TUNNEL_CAPACITY = CARS_COUNT / 2;
 
     public static void main(String[] args) {
 
         ExecutorService executor = Executors.newFixedThreadPool(CARS_COUNT);
-        CyclicBarrier cbStart = new CyclicBarrier(CARS_COUNT + 1);    //Барьер для метода подготовки к гонкам
-        CountDownLatch cdlStart = new CountDownLatch(CARS_COUNT);    //защелка для мейнПотока на Старт
-        CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT);  //защелка для мейнПотока на Финиш
-        Semaphore smp = new Semaphore(TUNNEL_CAPACITY, true);  //семафор для тоннеля проходимость TUNNEL_CAPACITY, в порядке очереди
+        final CyclicBarrier cbStart = new CyclicBarrier(CARS_COUNT + 1);    //Барьер для метода подготовки к гонкам
+        final CountDownLatch cdlStart = new CountDownLatch(CARS_COUNT);    //защелка для мейнПотока на Старт
+        final CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT);  //защелка для мейнПотока на Финиш
+        final Semaphore smp = new Semaphore(TUNNEL_CAPACITY, true);  //семафор для тоннеля проходимость TUNNEL_CAPACITY, в порядке очереди
 
         race(executor, cbStart, cdlStart, cdlFinish, smp);
 
@@ -24,7 +24,7 @@ public class MainClass {
 
     private static void race(Executor executor, CyclicBarrier cbStart, CountDownLatch cdlStart, CountDownLatch cdlFinish, Semaphore smp) {
         System.err.println("ОБЪЯВЛЕНИЕ >>> ПОДГОТОВКА К ГОНКЕ <<< ОБЪЯВЛЕНИЕ");
-        Race race = new Race(new Road(60), new Tunnel(80, smp), new Road(40));
+        final Race race = new Race(new Road(60), new Tunnel(80, smp), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cbStart, cdlStart, cdlFinish);
@@ -36,6 +36,7 @@ public class MainClass {
 
         wait(cdlStart); // ждем пока все участники подготовятся
         System.err.println("ОБЪЯВЛЕНИЕ >>> ГОНКА НАЧАЛАСЬ <<< ОБЪЯВЛЕНИЕ");
+        System.out.println();
 
         try {
             cbStart.await();
